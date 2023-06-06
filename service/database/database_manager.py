59a6ba58b530d3971bbc8247bcd2ad700ec2664f
kafiirgie <kafiirgie20@gmail.com>
2023-06-06 14:39:50 +0700

    create gitignore

--- service/database/database_manager.py
+++ service/database/database_manager.py
@@ -1,25 +1,12 @@
-from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Use a service account.
 cred = credentials.Certificate('credentials.json')
-
 app = firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
 users_ref = db.collection('users')
-docs = users_ref.stream()
-
-# print(docs)
-for doc in docs:
-    print(doc.to_dict())
-    # print(f'{doc.id} => {doc.to_dict()}')
-
-app = FastAPI()
-
-
-@app.get("/")
-def index():
-    return {"hello": "FastAPI"}
+meals_ref = db.collection('meals')
+users_docs = users_ref.stream()
+meals_docs = meals_ref.stream()
--- service/firebase.py
+++
@@ -1,9 +0,0 @@
-# firebaseConfig = {
-#     "apiKey": "AIzaSyBnCn5IwBNqqkFWma-eiS2DFJOm34K7-w0",
-#     "authDomain": "kost-kater.firebaseapp.com",
-#     "projectId": "kost-kater",
-#     "storageBucket": "kost-kater.appspot.com",
-#     "messagingSenderId": "962954133251",
-#     "appId": "1:962954133251:web:e8c90794444104b7203336",
-#     "measurementId": "G-B01WLF1L6D"
-# }
